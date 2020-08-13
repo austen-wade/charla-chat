b719938bc7e5a5593083b5a04e2a2fe80c34ed4b
Austen Wade <austen@Austens-MacBook-Pro.local>
2020-08-12 23:43:21 -0600

    wrote sql to match db

--- sql/chatSchema.sql
+++ sql/chatSchema.sql
@@ -1,8 +1,21 @@
-CREATE  TABLE chats (
-  id SERIAL PRIMARY KEY NOT NULL
+CREATE TABLE chat_user (
+  user_id  uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+  handle VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE chat_user (
-  id SERIAL PRIMARY KEY NOT NULL,
-  handle VARCHAR(45) NOT NULL
+CREATE TABLE chat_space (
+  space_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+  space_name VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE chat_room (
+  room_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+  space_id uuid,
+  room_name VARCHAR(45) NOT NULL,
+  CONSTRAINT fk_chat_space FOREIGN KEY (space_id) REFERENCES chat_space(space_id)
+);
+
+CREATE TABLE messages(
+  message_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+  content VARCHAR(256) NOT NULL
 );
